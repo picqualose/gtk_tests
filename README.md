1a7b3409bb8f52c75316083b7f052cb28ef1bbf6
Olivia SERENELLI-PESIN <olivia.serenelli.pesin@gmail.com>
2019-04-15 11:01:01 +0200

    segment fault

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # gtk_tests
+
+Si vous cherchez a comprendre quelque chose a GTK, c'est pas vraiment le bon endroit
--- test.c
+++ test.c
@@ -3,23 +3,50 @@
 
 ////////////////// FONCTION //////////////////
 
+// Ici ce sont ce qu'on appelle les fonctions de RAPPEL c'est a dire des fonction qui se seront appellees par gtk_main
+
+
+// Quand il est appele, affiche le message note dans le terminal 
 void coucou (GtkWidget *widget, gpointer data)
 {
     g_print ("Et quand tu cliques j'apparais :D \n");
 
 }
+
+// Indique a GTK quoi faire quand le signal delete arrive (fermer la fenetre via IG.
+// TRUE = on ferme la fenetre
+// FALSE = on laisse la fenetre ouverte 
+gint delete_event(GtkWidget *widget, GdkEvent *event, gpointer data)
+{
+    g_print ("le signal delete_event est survenu.\n");
+    return (FALSE);
+}
+
+//Fonction permettant de quitter l'application (trivial)
+void destroy (GtkWidget *widget, gpointer data)
+{
+    gtk_main_quit ();
+}
+
 /////////////////// MAIN //////////////////
 
 int main (int argc, char *argv[])
     {
-
+    //DECLARATION DES VARIABLES
         //Declaration d'un Widget, ici une fenetre puis un bouton
         GtkWidget *window;
         GtkWidget *button;
-    
+
+    //TRUCS DE BASES    
         //Fonction appelee dans toutes appli GTK --> evalue parametre en ligne de commande et retournes a l'appli (param de base)
         gtk_init (&argc, &argv);
-    
+
+        //Permet de quitter la fenetre via gestionnaire fenetre --> lie avec fonction de rappel 
+        //destroy 
+           gtk_signal_connect (GTK_OBJECT (window), "destroy",
+                        GTK_SIGNAL_FUNC (destroy), NULL);
+
+
         //Creation de la fenetre 
         window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
         //Affichage de la fenetre 
@@ -30,14 +57,24 @@ int main (int argc, char *argv[])
         //Creation d'un nouveau bouton 
         button = gtk_button_new_with_label ("Je suis un bouton");
         
+        //Le bouton appelle la fonction "coucou" si on clique dessus
         gtk_signal_connect (GTK_OBJECT (button), "clicked", GTK_SIGNAL_FUNC(coucou), NULL);
         
+        //On peut aussi lui dire d'appeler une autre fonctions en meme temps, 
+        //ici fermer la fenetre en plus
+        gtk_signal_connect_object (GTK_OBJECT (button), "clicked",
+                               GTK_SIGNAL_FUNC (gtk_widget_destroy),
+                               GTK_OBJECT (window));
+
+
         //On met le bouton dans la fenetre 
         gtk_container_add (GTK_CONTAINER (window), button);
 
         gtk_widget_show (button);
         gtk_widget_show (window);
 
+        // On affiche d'abord le "window" pour viter de voir d'abord la fenetre puis le bouton mais bien les deux
+        // en meme temps 
 
         gtk_main ();
     
